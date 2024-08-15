08a9f4ef3a1eae1791ab56420fcb452d16a2a492
Wilfred <700024165@student.curtin.edu.my>
2024-08-15 21:11:39 +0800

    express added

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require("express");
-// eslint-disable-next-line no-unused-vars
-// const bodyParser = require('body-parser');
-const path = require("path");
+// Import ES module syntax
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Initialize Express application
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "build")));
+// Get the current directory of the module (ES module equivalent of __dirname)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// This route serves the React app
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, 'build')));
 
+// Serve the React app on all routes
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')));
+
+// Start listening on the specified port
 app.listen(port, () => console.log(`Server listening on port ${port}`));
