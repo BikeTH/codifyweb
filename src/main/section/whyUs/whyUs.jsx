6b95ce2b4944938b9540d581647713206f0704e9
Wilfred Chong OM OSE <wilfredchong@wilfredchong-MacBook.Air>
2024-09-04 16:16:29 +0800

    why refine

--- src/main/section/whyUs/whyUs.jsx
+++ src/main/section/whyUs/whyUs.jsx
@@ -6,44 +6,38 @@ const chooseUs = [
     {
         id: 1,
         icon: <FaCoins />,
-        title: "Affordable packages",
-        description: "Get top-notch development services at competitive prices. Our packages are designed to fit various budgets without compromising on quality.",
-        color: "var(--warm-neon-yellow)",
+        title: "Affordable Packages",
+        description: "High-quality development services at budget-friendly prices.",
     },
     {
         id: 2,
         icon: <FaBolt />,
-        title: "Efficient development process",
-        description: "Experience a streamlined development process that saves you time and accelerates your project’s delivery. We ensure efficiency at every step.",
-        color: "var(--warm-neon-orange)",
+        title: "Efficient Process",
+        description: "Fast, streamlined development to speed up project delivery.",
     },
     {
         id: 3,
         icon: <FaPenNib />,
         title: "Custom Design",
-        description: "Receive a tailor-made design that reflects your brand’s unique identity. We create custom solutions that set you apart from the competition.",
-        color: "var(--warm-neon-blue)",
+        description: "Unique designs tailored to your brand's identity.",
     },
     {
         id: 4,
         icon: <FaMapLocationDot />,
-        title: "SEO target specific geographic area",
-        description: "Boost your online presence with SEO strategies that target your desired geographic area, driving more local traffic to your site.",
-        color: "var(--warm-neon-green)",
+        title: "Targeted SEO",
+        description: "Boost local traffic with geo-specific SEO strategies.",
     },
     {
         id: 5,
         icon: <FaUsersGear />,
-        title: "Troubleshooting Support Team",
-        description: "Benefit from our dedicated support team, ready to resolve any issues promptly and keep your website or app running smoothly.",
-        color: "var(--warm-neon-red)",
+        title: "Expert Support",
+        description: "Quick resolution of issues with our dedicated support team.",
     },
     {
         id: 6,
         icon: <FaServer />,
-        title: "Own I.T Server",
-        description: "Enjoy reliable performance with our own dedicated I.T. servers, ensuring your website and applications are always fast and secure.",
-        color: "var(--warm-neon-purple)",
+        title: "Reliable Servers",
+        description: "Fast, secure performance VPS servers scale your needs",
     },
 ]
 
@@ -51,15 +45,12 @@ export default function WhyUs(){
     return(
         <>
         <div className="ITconsult-why-us">
-            <h1 style={{textAlign:"center"}}>Why Choose Us?</h1>
+            <h1 style={{textAlign:"center"}}>What Sets Us Apart</h1>
             <div className="ITconsult-why-us-arrangement">
             {
                 chooseUs.map(data => (
                     <div className="ITconsult-why-us-content" key={data.id}>
-                        <div className="ITconsult-why-us-content-header">
-                            <h2 style={{color:data.color}}>{data.icon}</h2>
-                            <h3 style={{color:data.color, marginTop:"0"}}>{data.title}</h3>
-                        </div>
+                        <h5 style={{marginTop:"0"}}>{data.icon}{data.title}</h5>
                         <p>{data.description}</p>
                     </div>
                 ))}
