9d6d6cf5a71c596cf784a12f35feefd4de40f90f
Wilfred <700024165@student.curtin.edu.my>
2024-09-05 23:31:54 +0800

    logo added with contact sanitize

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import ITconsult from './main/ITconsult'
-import logo from '../public/logo.svg'
 
 export default function App() {
   return (
     <>
-      <img src={logo} alt="Logo" />
       <ITconsult/>
     </>
   )
