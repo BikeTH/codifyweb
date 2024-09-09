06daec68ab1cb88942904762ecfa21d1b0387026
Wilfred <700024165@student.curtin.edu.my>
2024-09-09 20:17:48 +0800

    carbon web added

--- src/main/section/important/note.jsx
+++ src/main/section/important/note.jsx
@@ -28,35 +28,40 @@ export default function Note() {
     const { ref: benefitRef, inView: benefitInView } = useIntersectionObserver({ threshold: 0.1 }, 100);
     const { ref: carbonRef, inView: carbonInView} = useIntersectionObserver({threshold:0.1}, 100);
 
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = "https://unpkg.com/website-carbon-badges@1.1.3/b.min.js";
-        script.async = true;
-        document.body.appendChild(script);
-
-        // Cleanup script when component unmounts
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
-
     return (
         <div className="note">
-            <div className={`note-craft ${craftInView ? 'animate' : ''}`} ref={craftRef}>
+            <div className={`note-craft ${craftInView ? 'animate' : 'paused'}`} ref={craftRef}>
                 <h1>Our Products Crafted with</h1>
                 <div>
                     <p style={{color:"var(--second-color)"}}>// Pure Code</p>
                 </div>
             </div>    
-            <div className={`note-why ${whyInView ? 'animate' : ''}`} ref={whyRef}>
+            <div className={`note-why ${whyInView ? 'animate' : 'paused'}`} ref={whyRef}>
                 <h2>Less</h2>
-                <div className={`note-why-support ${whyInView ? 'animate' : ''}`} ref={whyRef}>
+                <div className={`note-why-support ${whyInView ? 'animate' : 'paused'}`} ref={whyRef}>
                     <p style={{margin: 0, color:"var(--second-color)"}}>// Plugin</p>
                     <p style={{margin: 0, color:"var(--second-color)"}}>// Libraries</p>
                 </div>
             </div>
-            <div id="wcb" className={`carbonbadge ${carbonInView ? 'animate' : ''} `} ref={carbonRef}></div>
-            <div className={`note-benefit ${benefitInView ? 'animate' : ''}`} ref={benefitRef}>
+            <div className={`carbonBadge ${carbonInView ? 'animate' : 'paused'}`} ref={carbonRef}>
+                <div className="carbonBadge-arrangement">
+                    <div style={{border: "1px solid var(--border)", paddingLeft:"8px", paddingRight:"8px", borderTopLeftRadius:"20px", borderBottomLeftRadius:"20px"}}>
+                        <p style={{fontSize:"1.75vmin"}}>0.04g of CO2/view</p>
+                    </div>
+                    <div style={{border: "1px solid var(--border)", paddingLeft:"8px", paddingRight:"8px", borderTopRightRadius:"20px", borderBottomRightRadius:"20px"}}>
+                    <a 
+                        href="https://www.websitecarbon.com/" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        style={{ textDecoration: 'none' }} // This removes the default underline for links
+                    >
+                        <p style={{fontSize:"1.75vmin", color:"var(--seo)"}}>Website Carbon</p>
+                    </a>
+                    </div>
+                </div>
+                <p style={{margin: 0, fontSize:"1.75vmin", color:"var(--second-color)"}}>Cleaner than 96% of pages tested</p>
+            </div>
+            <div className={`note-benefit ${benefitInView ? 'animate' : 'paused'}`} ref={benefitRef}>
                 <h5 style={{color:"var(--second-color)"}}>Making it</h5>
                 <p className={`benefit-text ${showBenefit ? 'fade-in' : 'fade-out'}`}>{currentBenefit}</p>
             </div>
